2310757a43ddc79115474ab67621b945d9a0cf73
Mathieu Cherpitel <mc2091@hw.ac.uk>
2023-04-08 16:57:11 +0100

    done setuping for sunburst

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Loading and cleaning co2_emissions.csv
+// Loading and cleaning co2_emissions.csv (Used in choropleth.js)
 const co2_emissions = d3.csv("../data/co2_emission.csv", function (d) {
   if (parseInt(d["Annual CO₂ emissions (tonnes )"]) > 0)
     return {
@@ -8,7 +8,7 @@ const co2_emissions = d3.csv("../data/co2_emission.csv", function (d) {
     };
 });
 
-// Loading and cleaning feed_food.csv
+// Loading and cleaning feed_food.csv (Used in butterfly.js)
 const feed_food = d3.csv("../data/feed_food.csv", function (d) {
   if (parseInt(d["Y2013"]) > 0 && d["Element"] == "Food") {
     return {
@@ -20,7 +20,7 @@ const feed_food = d3.csv("../data/feed_food.csv", function (d) {
   }
 });
 
-// Loading and cleaning food_production.csv
+// Loading and cleaning food_production.csv (Used in donut.js)
 const food_production = d3.csv("../data/food_production.csv", function (d) {
   return {
     Product: d["Food product"],
@@ -32,6 +32,9 @@ const food_production = d3.csv("../data/food_production.csv", function (d) {
     Transport: parseFloat(d["Transport"]),
   };
 });
+
+// Loading hierarchy_co2_emission.json (Used in sunburst.js)
+const hierarchy_co2_emission = d3.json("../data/hierarchy_co2_emission.json");
 
 // Section 1
 donut();
--- js/sunburst.js
+++ js/sunburst.js
@@ -1,24 +1,5 @@
 function sunburst() {
-  food_production.then((d) => {
+  hierarchy_co2_emission.then((d) => {
     console.log(d);
   });
 }
-
-var data = {
-  name: "Feed animals",
-  value: 19.5,
-  childrens: [
-    {
-      name: "Beef (beef herd)",
-      value: 1.9,
-    },
-    {
-      name: "Beef (dairy herd)",
-      value: 2.5,
-    },
-    {
-      name: "Lamb & Mutton",
-      value: 2.4,
-    },
-  ],
-};
